4ef7974632e9e84b77842d6a43b33279ab3043d1
Jvcabrer <jvcabrer@espol.edu.ec>
2022-08-13 00:00:28 -0500

    Top20Ventas
    Intento

--- backend/src/controllers/keys.js
+++ backend/src/controllers/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: 'mysql',
+        password: 'SQLserver',
         database: 'stores'
     }
 };
--- frontend/src/components/Top20Ventas.js
+++ frontend/src/components/Top20Ventas.js
@@ -1,21 +1,66 @@
+import CanvasJSReact from '../canvasjs.react';
 import React, { Component } from 'react'
 import axios from "axios"
 import { backend } from '../App'
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export default class Top20Ventas extends Component {
-    render() {
-      return (
-        <p>Hola</p>
+  options;
+
+  render() {
+    return (
+      <div className="container">
+      <div>
+        <CanvasJSChart options={this.options}
+        /* onRef = {ref => this.chart = ref} */
+        />
+      </div>
+      </div>
+    );
+  }
+
+  state;
+  async getTOP20Ventas() {
+    const res = await axios.get(backend.host + ':' + backend.port + '/tiendas/top20Ventas');
+    this.setState({ users: res.data });
+    this.options.data[0].dataPoints = this.state.users.map(user => ({
+      label: `${user.Store_ID}`
+      , y: user.Store_Sales
+    }))
+
+  }
+  async componentDidMount() {
+    await this.getTOP20Ventas();
+    console.log(this.state.users);
+  }
+
+  constructor(){
+    super();
+    this.state={
+      users: [],
+      Store_ID: '',
+      Store_Area: '',
+    }
   
-      )
+    this.options={
+      title: {
+        text: "Ranking 20 ventas"
+      },
+      axisX: {
+				title: "Tiendas",
+				reversed: true,
+			},
+			axisY: {
+				title: "Ventas",
+
+			},
+      data: [{
+        type: "bar",
+        dataPoints: [
+  
+        ]
+      }]
     }
-    
-    async getTOP20Ventas() {
-      const res = await axios.get(backend.host + ':' + backend.port + '/tiendas/top20Ventas');
-      //this.setState({ users: res.data });
-    }
-    async componentDidMount() {
-      await this.getTOP20Ventas();
-      //console.log(this.state.users);
-    }
+  }
 }
